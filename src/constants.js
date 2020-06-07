fd08bdb45deee94a9c35f3c96b6f3ba277e7fee9
koby <kobybitonu@gmail.com>
2020-06-07 18:00:16 +0300

    updating api key

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-const API_KEY = "UbHndA3AAPOYTyzq0dORKXpxYRpMLfut";
+const API_KEY = "GU9VzyUn82gkZWqcpBppP97D1gyWWjjY";
 const AUTO_COMPLETE_LOCATION = `http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=${API_KEY}`;
 const DAILY_FORECASTS = `http://dataservice.accuweather.com/forecasts/v1/daily/5day/search?apikey=${API_KEY}`;
 const CURRENT_CONDITION = `http://dataservice.accuweather.com/currentconditions/v1/search?apikey=${API_KEY}`;
