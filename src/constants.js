b0a5fe8ced0acc7cd9c8329ba4e17fd4ccbadf35
koby <kobybitonu@gmail.com>
2020-06-07 18:02:44 +0300

    updating api url to https

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 const API_KEY = "GU9VzyUn82gkZWqcpBppP97D1gyWWjjY";
-const AUTO_COMPLETE_LOCATION = `http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=${API_KEY}`;
-const DAILY_FORECASTS = `http://dataservice.accuweather.com/forecasts/v1/daily/5day/search?apikey=${API_KEY}`;
-const CURRENT_CONDITION = `http://dataservice.accuweather.com/currentconditions/v1/search?apikey=${API_KEY}`;
+const AUTO_COMPLETE_LOCATION = `https://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=${API_KEY}`;
+const DAILY_FORECASTS = `https://dataservice.accuweather.com/forecasts/v1/daily/5day/search?apikey=${API_KEY}`;
+const CURRENT_CONDITION = `https://dataservice.accuweather.com/currentconditions/v1/search?apikey=${API_KEY}`;
 
 export { AUTO_COMPLETE_LOCATION, DAILY_FORECASTS, CURRENT_CONDITION }; // export constant to be able to use them in apiDataService
